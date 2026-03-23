3841ebbef5f6a287428cf8e8b2645bd98a5be5b1
omar-hossam <omarhossam50@proton.me>
2026-03-24 00:03:21 +0200

    feat: add while loop & if statement

--- calculator.cpp
+++ calculator.cpp
@@ -4,47 +4,58 @@
 int main() {
     double num1, num2;
     char operation;
+    char calc_again;
     
     std::cout << "Simple Calculator" << std::endl;
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
     
-    std::cout << "Select operation (+, -, *, /, %, ^): ";
-    std::cin >> operation;
-    
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
-    
-    printf("%.2f %c %.2f = ", num1, operation, num2);
-    
-    switch(operation) {
-        case '+':
-            std::cout << num1 + num2;
+    while (true) {
+        std::cout << "Enter first number: ";
+        std::cin >> num1;
+        
+        std::cout << "Select operation (+, -, *, /, %, ^): ";
+        std::cin >> operation;
+        
+        std::cout << "Enter second number: ";
+        std::cin >> num2;
+        
+        printf("%.2f %c %.2f = ", num1, operation, num2);
+        
+        switch(operation) {
+            case '+':
+                std::cout << num1 + num2;
+                break;
+            case '-':
+                std::cout << num1 - num2;
+                break;
+            case '*':
+                std::cout << num1 * num2;
+                break;
+            case '/':
+                if(num2 != 0) {
+                    std::cout << num1 / num2;
+                } else {
+                    std::cout << "Error: Division by Zero!";
+                }
+                break;
+            case '%':
+                std::cout << remainder(num1, num2);
+                break;
+            case '^':
+                std::cout << pow(num1, num2);
+                break;
+            default:
+                std::cout << "Invalid operation!";
+        }
+        
+        std::cout << std::endl << std::endl;
+        
+        std::cout << "Calc again (Y,n)? ";
+        std::cin >> calc_again;
+        
+        if ((char)tolower(calc_again) != 'y') {
+            std::cout << "Thanks for using our calculator :)" << std::endl;
             break;
-        case '-':
-            std::cout << num1 - num2;
-            break;
-        case '*':
-            std::cout << num1 * num2;
-            break;
-        case '/':
-            if(num2 != 0) {
-                std::cout << num1 / num2;
-            } else {
-                std::cout << "Error: Division by Zero!";
-            }
-            break;
-        case '%':
-            std::cout << remainder(num1, num2);
-            break;
-        case '^':
-            std::cout << pow(num1, num2);
-            break;
-        default:
-            std::cout << "Invalid operation!";
+        }
     }
-    
-    std::cout << std::endl;
-    
     return 0;
 }
